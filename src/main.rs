06371a461a3a4ae6d46957d8264fdead708dbda1
Abelardo Mendoza <abelardo22.9@gmail.com>
2025-03-13 22:51:19 -0600

    add breakpoint interrupt handler through x86_64

--- src/main.rs
+++ src/main.rs
@@ -24,9 +24,13 @@ fn panic(info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
     println!("Hello World {}", "!");
 
+    rustos::init();
+    x86_64::instructions::interrupts::int3();
+
     #[cfg(test)]
     test_main();
 
+    println!("It did not crash!");
     loop {}
 }
 
